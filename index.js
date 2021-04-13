375017f468949c37fde476c1a6cb2c2dfa286436
Alejandro Vera <aleverasj@gmail.com>
2021-04-13 15:38:05 -0300

    primer trabajo con NODE

---
+++ index.js
@@ -0,0 +1,80 @@
+const http = require("http");
+const url = require ("url");
+const StringDecoder = require('string_decoder').StringDecoder;
+
+    //1.obtener la url desde el objeto request// ok
+const server = http.createServer((req, res)=>{
+    const urlActual = req.url;
+    const urlParseada = url.parse(urlActual, true);
+
+    //2.obtener la ruta//ok
+    const ruta = urlParseada.pathname;
+
+    //3. quitar slash
+    let rutaLimpia = ruta.replace(/^\/+|\/+$/g,"");
+
+    //3.1 obtener metodo http
+    const metodo = req.method;
+
+    //3.2 obtener variables de query url
+    const { query = {}} = urlParseada;
+
+    //3.3 obtener headers
+    const { headers = {} } = req;
+
+    //3.4 obtener payload, en el caso de haber uno
+    const decoder = new StringDecoder("utf-8");
+    let buffer ="";
+
+    //3.4.1 ir acumulando la data cuando el request reciba un payload
+    req.on("data", (data)=>{
+        buffer += decoder.write(data);
+    });
+
+    //3.4.2 terminar de acumular datos y decirle al decoder que finalice
+    req.on("end", ()=>{
+        buffer += decoder.end();
+
+         //3.5 ordenar la data del request
+         let data = {
+             ruta: rutaLimpia,
+             query,
+             metodo,
+             headers,
+             payload: buffer,
+         };
+
+         //3.6 elegir el manejador dependiendo de la ruta y asignarle la funcion que el enrutador tiene
+         let handler;
+         if (rutaLimpia && enrutador[rutaLimpia]){
+             handler = enrutador[rutaLimpia];
+         } else {
+             handler = enrutador.noEncontrado; //el problema esta aca
+         };
+
+           //4. ejecutar handler para enviar la respuesta
+           if (typeof handler === 'function' ){
+            handler(data, (statusCode = 200, mensaje)=>{
+               let respuesta = JSON.stringify(mensaje);
+               res.writeHead(statusCode);
+               //linea donde realmente ya estamos respondiendo a la aplicacion cliente 
+               res.end(respuesta);
+            })
+        }
+       
+    });
+});
+const enrutador = {
+    ruta: (data, callback) =>{
+        callback(200, {mensaje: "esta es /ruta"});
+    },
+    usuarios: (data, callback) =>{
+        callback(200, [{nombre: "usuario 2"}, {nombre: "usuario 1"}]);
+    },
+    noEncontrado: (data, callback) =>{
+        callback(404, {mensaje: "no encontrado"});
+    }
+};
+server.listen(5000,()=>{
+    console.log("el servidor esta escuchando peticiones en http://localhost:5000/");
+});
