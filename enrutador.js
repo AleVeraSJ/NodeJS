bf166428960dee9e9b73d8e24c7a7b77b09c62c5
Alejandro Vera <aleverasj@gmail.com>
2021-05-18 10:25:08 -0300

    refactorizacion

--- enrutador.js
+++ enrutador.js
@@ -1,36 +1,15 @@
+const recursos = require("./recursos");
+const mascotas = require("./rutas/mascotas");
+const veterinarias = require("./rutas/veterinarias");
+
+
+
 module.exports = {
     ruta: (data, callback) =>{
         callback(200, {mensaje: "esta es /ruta"});
     },
-    /*usuarios: (data, callback) =>{
-        callback(200, [{nombre: "usuario 2"}, {nombre: "usuario 1"}]);
-    },*/
 
-    mascotas:{
-        get: (data, callback) =>{
-            if (data.indice){
-                if(global.recursos.mascotas[data.indice]){
-                    return  callback(200,global.recursos.mascotas[data.indice]);
-                }
-                return callback(404 ,{mensaje: `mascota con indice ${data.indice} no encontrada`});
-            }
-            callback(200,global.recursos.mascotas);
-        },
-        post: (data, callback) =>{
-            global.recursos.mascotas.push(data.payload);
-            callback(201, data.payload);
-        },
-        put: (data, callback) =>{
-            if (data.indice !== "undefined"){
-                if(global.recursos.mascotas[data.indice]){
-                    global.recursos.mascotas[data.indice] = data.payload;
-                    return  callback(200,global.recursos.mascotas[data.indice]);
-                }
-                return callback(404 ,{mensaje: `mascota con indice ${data.indice} no encontrada`});
-            }
-            callback(400, {mensaje: "indice no enviado"});
-        },
-    },
+    mascotas: mascotas(recursos.mascotas),
     noEncontrado: (data, callback) =>{
         callback(404, {mensaje: "no encontrado"});
     }
--- recursos.js
+++ recursos.js
@@ -1,8 +1,15 @@
 module.exports = {
     mascotas: [
-        {tipo:"perro", nombre: "Rocco", dueño:"Ale" },
-        {tipo:"perro", nombre: "Rocco", dueño:"Ale" },
-        {tipo:"perro", nombre: "Rocco", dueño:"Ale" },
-        {tipo:"perro", nombre: "Rocco", dueño:"Ale" }
+        {tipo:"perro", nombre: "Rocco0", dueño:"Ale" },
+        {tipo:"perro", nombre: "Rocco1", dueño:"Ale" },
+        {tipo:"perro", nombre: "Rocco2", dueño:"Ale" },
+        {tipo:"perro", nombre: "Rocco3", dueño:"Ale" }
+    ],
+
+    veterinarias: [
+        {nombre: "Ale", apellido: "Vera", documento: "40592085" },
+        {nombre: "Jesus", apellido: "Barbero", documento: "34355665" },
+        {nombre: "Rama", apellido: "Ortega", documento: "43543435" },
+        {nombre: "Mario", apellido: "Abad", documento: "45647598" }
     ]
 };
