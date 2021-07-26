c8783144333e758a395419da204ef6241c77b0dd
Alejandro Vera <aleverasj@gmail.com>
2021-07-26 15:24:49 -0300

    muchos cambios y una imagen para ir practicando

---
+++ frontend/js/consultas.js
@@ -0,0 +1,79 @@
+const listaConsultas = document.getElementById ("lista-consultas");
+
+
+/*  {mascota: 0, 
+        veterinaria: 0,  
+        fechaCreacion: new Date (),
+        fechaEdicion: new Date (), 
+        historia: "", 
+        diagnostico: ""
+        },*/
+
+
+
+
+let consultas = [];
+const url = "http://localhost:5000/consultas";
+
+async function listarConsultas() {
+    try {
+        const respuesta = await fetch (url);
+        const consultasDelServer = await respuesta.json();
+        if(Array.isArray(consultasDelServer)){
+        consultas = consultasDelServer;
+        }
+        if (respuesta.ok){
+            const htmlConsultas = consultas.map(
+                (consulta, indice) =>
+                `<tr>
+                    <th scope="row">${indice}</th>
+                    <td>${consulta.mascota}</td>
+                    <td>${consulta.veterinaria}</td>
+                    <td>${consulta.diagnostico}</td>
+                    <td>${consulta.fechaCreacion}</td>
+                    <td>${consulta.fechaEdicion}</td>
+                    <td>
+                    <div class="btn-group" role="group" aria-label="Basic example">
+                        <button type="button" class="btn btn-primary">Editar</button>
+                    </div>
+                    </td>
+                    </tr>
+                <tr>`
+             )
+             .join("");
+             listaConsultas.innerHTML = htmlConsultas;
+
+        
+        }
+
+
+
+    } catch (error) {
+        throw error;
+    }
+}
+listarConsultas();
+
+
+
+
+
+
+
+
+
+
+
+`<tr>
+<th scope="row">2</th>
+<td>Jacob</td>
+<td>Thornton</td>
+<td>@fat</td>
+<td>
+  <div class="btn-group" role="group" aria-label="Basic example">
+    <button type="button" class="btn btn-primary">Editar</button>
+    <button type="button" class="btn btn-danger">Eliminar</button>
+  </div>
+</td>
+</tr>
+<tr>`
